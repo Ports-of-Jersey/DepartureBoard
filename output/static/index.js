045437c8a095a8501b94c1b98b1e6d103130fbe7
patrickhogan93 <patrickhogan93@gmail.com>
2020-06-08 14:04:07 +0100

    .

--- output/static/index.js
+++ output/static/index.js
@@ -2,7 +2,7 @@ var date = new Date();
 
 function setDate() {
   let date = new Date();
-  date = date.toLocaleString('en-GB', { timeZone: 'UTC' });
+  date = date.toLocaleString('en-GB', { timeZoneName: 'short' });
   let dateElement = document.getElementById('footer-date');
   dateElement.innerHTML = date;
 }
