d645b7bda7cfc63f606e824bcd439c1582a74755
patrickhogan93 <patrickhogan93@gmail.com>
2020-06-22 15:45:42 +0100

    .

--- 1_grabdata.py
+++ 1_grabdata.py
@@ -2,10 +2,10 @@ from zeep.plugins import HistoryPlugin
 from zeep import Client
 from lxml import etree
 
-wsdl_url = 'https://podbwebservice.ports.je/AIDXQueryFlights/Services/RequestFlightService.svc?wsdl'
+wsdl_url = 'https://podbwebservice.ports.je:8443/AIDXQueryFlights/Services/RequestFlightService.svc?wsdl'
 service = 'RequestFlightService'
 port = 'BasicHttpsBinding_IRequestFlightService'
-userID = 'POJInternal123'
+userID = 'POJWebInternalTest456'
 
 history = HistoryPlugin()
 
@@ -13,11 +13,11 @@ client = Client(wsdl_url, plugins=[history])
 
 service2 = client.bind(service, port)
 
-data = service2.flightRequest(userID = userID, fullRefresh = False)
-
-service2.flightRequest(userID = userID, fullRefresh = False)
+service2.flightRequest(userID = userID, fullRefresh = True)
 flight_xml = etree.tostring(history.last_received["envelope"], encoding="unicode", pretty_print=True)
 
-f = open("flightinfo.xml", "a")
+print(flight_xml)
+
+f = open("flightinfo.xml", "w")
 f.write(flight_xml)
 f.close()
